7cb2d6fb7627997aacd7090616553320d04a0ced
juhwan1014 <jmoon63@my.bcit.ca>
2022-10-20 00:52:39 -0700

    banner styling

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -9,20 +9,31 @@ const HeroBanner = () => {
     mt: {lg: '212px', xs: '70px'},
     ml: {sm: '50px'}
    }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26x">
+    <Typography color="#046ED8" fontWeight="600" fontSize="26x">
         Fitness Club
     </Typography>
     <Typography fontWeight={700}
     sx={{fontSize:{lg: '44px', xs:'40px'}}}>
         Sweat, Smile <br /> and Repeat 
     </Typography>
-    <Typography fontSize="22px" LineHeight="35px" mb={2}>
+    <Typography fontSize="22px" LineHeight="35px" mb={4}>
         Check out the most effective exercises
     </Typography>
-    <Button variant="contained" color="error" href="#exercises">
+    <Button variant="contained" color="primary" href="#exercises">
         Explore Exercises
     </Button>
-    <img src={HeroBannerImage} alt="Banner" className="hero-banner-img"/>
+    <Typography
+     fontWeight={600}
+     color="#046ED8"
+     sx={{
+        opacity: 0.1,
+        display: {lg: 'block', xs: 'none'}
+     }}
+     fontSize = "200px"
+    >
+        Exercise
+    </Typography>
+    <img src={HeroBannerImage} alt="Banner" className="hero-banner-img" />
    </Box>
   )
 }

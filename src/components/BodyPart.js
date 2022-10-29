10b9ac2c601bb92484fe46aaf9de628c7f06f5a0
juhwan1014 <jmoon63@my.bcit.ca>
2022-10-28 21:54:40 -0700

    scroll menu of bodyPart

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -2,14 +2,25 @@ import React from 'react'
 import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
 
-const BodyPart = () => {
+const BodyPart = ({item, setBodyPart, bodyPart}) => {
   return (
     <Stack 
       type="button"
       alignItems="center"
       justifyContent="center"
       className="bodyPart-card"
+      sx={ {
+        borderTop:  bodyPart === item ?  '4px solid #046ED8': '',
+        backgroundColor: '#fff',
+        borderBottomLeftRadius: '20px',
+        width: '270px',
+        height:'280px',
+        cursor: 'pointer', 
+        gap:'47px'
+        } }
+    
       
+
     >
         <img src={Icon} alt="dumbbell" style={{width: '40px', height:'40px'}}/>
     </Stack>
